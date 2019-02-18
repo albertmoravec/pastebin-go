65df5823199760bb61470a19b96f389858ec3488
Albert Moravec <albert.moravec@gmail.com>
2019-02-18 17:26:18 +0100

    add savePaste call - I'm an idiot

--- pastebin.go
+++ pastebin.go
@@ -305,6 +305,8 @@ func documentHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
+		savePaste(p)
+
 		w.Header().Set("Content-Type", "application/json")
 		w.Write([]byte("{\"key\":\"" + p.Url + "\"}"))
 
